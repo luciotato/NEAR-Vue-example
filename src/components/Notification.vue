a4bac7490402ac46f6f8f7fdebd87f646a897e41
Lucio Tato <luciotato@gmail.com>
2020-08-15 21:38:05 -0300

    inital commit to github

---
+++ src/components/Notification.vue
@@ -0,0 +1,31 @@
+﻿<template>
+    <div class="notification">
+        <aside>
+            <a target="_blank" rel="noreferrer" v-bind:href="urlPrefix + '/' + accountId">
+                {{ accountId }}
+            </a>
+            {{ msg }}
+            in contract: <a target="_blank" rel="noreferrer" v-bind:href="urlPrefix + '/' + contractId">
+                {{ contractId }}
+            </a>
+            <footer>
+                <div>✔ Succeeded</div>
+                <div>Just now</div>
+            </footer>
+        </aside>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Notification',
+    props: {
+        accountId: String,
+        contractId: String,
+        msg: String,
+    },
+    computed: {
+        urlPrefix: () => "https://explorer." + window.networkId + ".near.org/accounts"
+    }
+}
+</script>
